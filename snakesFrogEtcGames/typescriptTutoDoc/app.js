31cb1c8c72102a77057039951e3ef1355c112feb
zanusurp <j.chan.h1210@gmail.com>
2021-01-23 00:55:59 +0900

    tyscript enum array objewct any
    tyscript enum array objewct any

--- snakesFrogEtcGames/typescriptTutoDoc/app.js
+++ snakesFrogEtcGames/typescriptTutoDoc/app.js
@@ -1,3 +1,4 @@
+//typescript Academind
 function add(n1, n2, showResult, phrase) {
     if (typeof n1 !== 'number' && typeof n2 !== 'number') {
         throw new Error('Incorrect Input ! ');
@@ -17,9 +18,59 @@ var number2 = 2.9;
 var printResult = true;
 var resultPhrase = 'result is : ';
 add(number1, number2, printResult, resultPhrase);
+//object : generic
 var person = {
     name: 'Maxim',
     nickname: 'tajan',
     age: 30
 };
-console.log(person.nickname);
+//{} 이게 오브젝트 표현인데 안에 파라미터를 넣으면 각개 부를 수 있도록 바뀜
+var person1 = {
+    name: 'Maxim',
+    nickname: 'tajan',
+    age: 30,
+    role: [2, 'podo']
+};
+//그냥 이렇게 하라함
+var person2 = {
+    name: 'max',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author'] //튜플
+};
+person2.role.push('admin');
+//person2.role[1] = 10;
+console.log(person);
+console.log(person1.age);
+console.log(person2.age);
+//Array
+console.log(person2.hobbies[1]);
+var favoriteActivities;
+favoriteActivities = ['Sports']; //스트링으로 배열 했기 때문에 이전처럼 아무거나 안들어감
+var favoriteActivities1; //이건 아무거나 들어감 any타입
+favoriteActivities1 = ['aa', 1, true, person];
+for (var _i = 0, _a = person2.hobbies; _i < _a.length; _i++) { //foreach 같다 
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+    //console.log(hobby.map()); //error
+}
+//Enums 이넘
+//const ADMIN = 0;
+//const  READ_ONLY = 1;
+//const AUTHOR = 2;
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+var person3 = {
+    name: 'mm',
+    age: 20,
+    hobbies: ['ss', 'dd'],
+    role: Role.ADMIN //enum으로 쓸 예정 번호에 따른 역할 
+};
+if (person3.role === Role.ADMIN) {
+    console.log('role is ADMIN');
+}
+//ANY TYPE 애니 타입  : 특정 한 게 없다 
--- snakesFrogEtcGames/typescriptTutoDoc/app.ts
+++ snakesFrogEtcGames/typescriptTutoDoc/app.ts
@@ -1,3 +1,4 @@
+//typescript Academind
 function add(n1:number,n2:number,showResult:boolean, phrase:string){
     if(typeof n1 !== 'number' && typeof n2 !=='number'){
         throw new Error('Incorrect Input ! ');
@@ -17,10 +18,79 @@ const printResult = true;
 const resultPhrase = 'result is : ';
 add(number1,number2, printResult, resultPhrase);
 
-//object
-const person = {
+//object : generic
+const person :object= {
     name:'Maxim',
     nickname:'tajan',
     age: 30
 };
-console.log(person.nickname);
+//{} 이게 오브젝트 표현인데 안에 파라미터를 넣으면 각개 부를 수 있도록 바뀜
+const person1 :{
+    name:string;
+    nickname:string;
+    age:number;
+    role:[number,string]; //튜플 //Tuple 튜플 
+}= {
+    name:'Maxim',
+    nickname:'tajan',
+    age: 30,
+    role:[2,'podo']
+};
+//그냥 이렇게 하라함
+const person2 = {
+    name:'max',
+    age:30,
+    hobbies:['Sports','Cooking'],
+    role:[2,'author'] //튜플
+};
+person2.role.push('admin');
+//person2.role[1] = 10;
+
+
+console.log(person);
+console.log(person1.age);
+console.log(person2.age);
+//Array
+console.log(person2.hobbies[1]);
+
+let favoriteActivities:string[];
+favoriteActivities = ['Sports']; //스트링으로 배열 했기 때문에 이전처럼 아무거나 안들어감
+let favoriteActivities1:any[]; //이건 아무거나 들어감 any타입
+favoriteActivities1 = ['aa',1, true, person];
+
+for (const hobby of person2.hobbies){ //foreach 같다 
+    console.log(hobby.toUpperCase());
+    //console.log(hobby.map()); //error
+}
+
+//Enums 이넘
+//const ADMIN = 0;
+//const  READ_ONLY = 1;
+//const AUTHOR = 2;
+
+enum Role{
+    ADMIN, 
+    READ_ONLY,
+    AUTHOR
+}
+
+const person3 = {
+    name: 'mm',
+    age:20,
+    hobbies:['ss','dd'],
+    role:Role.ADMIN //enum으로 쓸 예정 번호에 따른 역할 
+}
+if(person3.role===Role.ADMIN){
+    console.log('role is ADMIN');
+}
+//ANY TYPE 애니 타입  : 특정 한 게 없다 
+
+
+
+
+
+
+
+
+
+
