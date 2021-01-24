622782018a7f31c916827be946d3081fe7cb1692
zanusurp <j.chan.h1210@gmail.com>
2021-01-25 00:50:50 +0900

    python end with turtle
    python end with turtle

---
+++ snakesFrogEtcGames/python-prac/turtle.py
@@ -0,0 +1,35 @@
+# import turtle
+# import random
+
+# def screenLeftClick(x,y):
+#     global r,g,b
+#     turtle.pencolor((r,g,b))
+#     turtle.pendown()
+#     turtle.goto(x,y)
+
+# def screenRightClick(x,y):
+#     turtle.penup()
+#     turtle.goto(x,y)
+
+# def screenMidClick(x,y):
+#     global r,g,b
+#     tSize=random.randrange(1,10)
+#     turtle.shapesize(tSize)
+#     r= random.random()
+#     g= random.random()
+#     b= random.random()
+
+
+# pSize = 10
+# r,g,b =0.0, 0.0,0.0
+
+
+# turtle.shape("turtle")
+# turtle.pensize(pSize)
+
+# turtle.onscreenclick(screenLeftClick,1)
+# turtle.onscreenclick(screenRightClick,2)
+# turtle.onscreenclick(screenMidClick,3)
+
+# turtle.done()
+#파이참에선 됨 
